822bf14ac5e48a1d64fe389b1258242a9cb13a9f
Katerina Boyer <katerina.boyer@tamu.edu>
2019-11-06 16:43:17 -0600

    clean up css and add user validations for signup

--- carpoolApplication/app/models/user.rb
+++ carpoolApplication/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates   :email,           presence: true, uniqueness: true
+  validates   :email,           presence: true,   format: { with: URI::MailTo::EMAIL_REGEXP },  uniqueness: true
   validates   :first_name,      presence: true
   validates   :last_name,       presence: true
-  validates   :uin,             presence: true
+  validates   :uin,             presence: true,   :numericality => {:only_integer => true}, length: { is: 9 },     uniqueness: true
   validates   :role,            presence: true
-  validates   :password_digest, presence: true
+  validates   :password,        presence: true,   confirmation: true, length: { minimum: 8 }
 end
