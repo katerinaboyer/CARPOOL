05191dba1171dc4150729c5b5f3daba78ca6cf19
Katerina Boyer <katerina.boyer@tamu.edu>
2019-11-06 12:05:07 -0600

    fix users method bug with Name

--- carpoolApplication/app/models/user.rb
+++ carpoolApplication/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  validates   :email, presence: true, uniqueness:  true
+
+  validates   :email,           presence: true, uniqueness: true
+  validates   :first_name,      presence: true
+  validates   :last_name,       presence: true
+  validates   :uin,             presence: true
+  validates   :role,            presence: true
+  validates   :password_digest, presence: true
 end
