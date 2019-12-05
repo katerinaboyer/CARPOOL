7a77b0e5b283808c0a988303326a05f961b5a5b3
EC2 Default User <ec2-user@ip-172-31-35-6.us-east-2.compute.internal>
2019-12-05 21:16:20 +0000

    fixing login bug

--- carpoolApplication/app/models/user.rb
+++ carpoolApplication/app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates   :email,           presence: true,   format: { with: URI::MailTo::EMAIL_REGEXP },  uniqueness: true
-  validates   :first_name,      presence: true,   :with => /[a-z]/
-  validates   :last_name,       presence: true,   :with => /[a-z]/
+  validates   :first_name,      format:{   :with => /\A[a-zA-Z]+\z/}
+  validates   :last_name,       format:{   :with => /\A[a-zA-Z]+\z/}
   validates   :uin,             presence: true,   :numericality => {:only_integer => true}, length: { is: 9 },     uniqueness: true
   validates   :role,            presence: true
   validates   :password,        presence: true,   confirmation: true, length: { minimum: 8 }
