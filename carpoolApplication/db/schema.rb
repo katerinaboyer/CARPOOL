161172dad69d2ba8a0b3aec0ba34f77e3804d397
Jon H <jon.hillier@mac.com>
2019-12-08 23:56:16 +0000

    seperated the rides into tables

--- carpoolApplication/db/schema.rb
+++ carpoolApplication/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191126142819) do
+ActiveRecord::Schema.define(version: 20191203190206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20191126142819) do
     t.datetime "application_received"
     t.string   "ride_status"
     t.text     "notes"
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
